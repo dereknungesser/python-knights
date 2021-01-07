8231e605d34338823e628f81640a6d99e9bfd51e
Dez <dez@dezs-mbp.attlocal.net>
2021-01-07 17:39:19 -0500

    phase 2 complete

--- path_finders.py
+++ path_finders.py
@@ -2,13 +2,13 @@ from tree import Node
 
 
 class KnightPathFinder:
-   def __init__(self, initial_cords):
+    def __init__(self, initial_cords):
        self._initial_cords = initial_cords
        self._root = Node(initial_cords)
        self._considered_postitions = set((initial_cords))
 
     def get_valid_moves(self, pos):
-        pos = (x, y)
+        (x, y) = pos
         new_moves = {
             (2, 1),
             (2, -1),
@@ -19,4 +19,21 @@ class KnightPathFinder:
             (-1, 2),
             (-1, -2),
         }
+        possible_moves = {
+            (x + i, y + j) for (i , j) in new_moves
+            if(x + i <= 8 and x + i >=0 and y + j <= 8 and y + j >= 0)
+        }
+
         return possible_moves
+
+    def new_move_positions(self, pos):
+        possible_moves = self.get_valid_moves(pos)
+
+        new_moves = possible_moves.difference(self._considered_postitions)
+
+        self._considered_postitions = self._considered_postitions.union(new_moves)
+        return new_moves
+
+
+finder = KnightPathFinder((0, 0))
+print(finder.new_move_positions((0, 0)))
