965373e7efa11eab92db8ead69738fd6cccb6863
Dez <dez@dezs-mbp.attlocal.net>
2021-01-08 13:22:31 -0500

    phase 3 complete

--- path_finders.py
+++ path_finders.py
@@ -34,8 +34,20 @@ class KnightPathFinder:
         self._considered_postitions = self._considered_postitions.union(new_moves)
         return new_moves
 
-    def build_move_tree(self):    
+    def build_move_tree(self):  
+        queue = [self._root]
+
+        while queue:
+            node = queue.pop(0)
+            moves = self.new_move_positions(node.value)
+            child_nodes = [Node(pos) for pos in moves]
+            for child_node in child_nodes:
+                node.add_child(child_node)
+            queue.extend(child_nodes)
+
 
 
 finder = KnightPathFinder((0, 0))
+finder.build_move_tree()
+print("This", finder._root.children)
 print(finder.new_move_positions((0, 0)))
